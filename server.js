52fbad3c6a72b2f6872270332489eae81614d25e
meatball <meatball@meatballs-MacBook-Pro.local>
2017-10-21 22:41:04 -0700

    another commit

--- server.js
+++ server.js
@@ -3,23 +3,29 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   // res.send('<h1>hello express!</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Hello world!',
-    currentYear: new Date().getFullYear()  
+    welcomeMessage: 'Hello world!'
   })
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
